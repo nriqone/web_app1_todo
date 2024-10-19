943a9745b503f2aa9cd3cba01b491c03087ee0f4
nrq <nriqone@gmai.com>
2024-10-19 15:32:19 +0200

    v1

--- web.py
+++ web.py
@@ -8,12 +8,9 @@ def add_todo():
 
 todos = functions.get_todos()
 
-# st.title("My todo App")
-# st.subheader("This is my todo app.")
-# st.write("This app ip")
-st.title("Tareas Irene - Horno")
-st.subheader("Claro, hay que hacer las cosa")
-st.write("Listemos qué hay que hacer")
+st.title("My todo App")
+st.subheader("This is my todo app.")
+st.write("This app ip")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,7 +19,6 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.rerun()
-        print(todo)
 
 value = st.text_input(label=" ", placeholder='Enter a todo',
                       on_change=add_todo, key='new_todo')
